edad104ad2d5dbe33df40b470ccdfc6e7ecd3328
Nimesh-Mathavadiya009 <mathavadiyanimesh@gmail.com>
2024-08-04 16:26:02 +0530

    all about is done

---
+++ src/componenets/index.js
@@ -0,0 +1,33 @@
+import Header from "./header/Header"
+import Footer from "./footer/Footer"
+import Container from './container/Container' 
+import Logo  from "./Logo"
+import LogoutBtn from "./header/LogoutBtn"
+import Button from "./Button"
+import RTE from "./RTE"
+import SignUp from "./SignUp"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayOut"
+import Input from "./Input"
+import Select from "./Select"
+import Skeleton from "./Skeleton"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    RTE,
+    SignUp,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    Input,
+    Select,
+    Skeleton
+}
